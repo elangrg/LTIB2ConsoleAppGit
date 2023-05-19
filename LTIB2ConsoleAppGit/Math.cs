9eea335424d2ddd6c2cfef1a18eb92f168dbcf2a
elangrg <45620769+elangrg@users.noreply.github.com>
2023-05-20 01:54:18 +0530

    Add New Math

---
+++ LTIB2ConsoleAppGit/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTIB2ConsoleAppGit
+{
+    public  class Math
+    {
+
+        public static int Add(int i, int j)
+        {
+
+            return i + j;
+
+        
+        }
+
+
+    }
+}
